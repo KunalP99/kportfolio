908e1eb0a02517ac0960cf1ccf6ce611f3589d99
KunalP99 <kunal_patel200@hotmail.co.uk>
2022-12-08 20:48:59 +0000

    More information in shopping cart project

--- src/components/projects/ShoppingCart.js
+++ src/components/projects/ShoppingCart.js
@@ -44,8 +44,9 @@ export default function ShoppingCart() {
               Using a mobile-first design approach, I created a shopping cart
               where the user can add items to a basket, fine tune the quantity
               of the items, remove items from the basket and once done, order
-              the item/s. I used React Router for this project, with 3 routes,
-              to gain familiarity with it.
+              the item/s. I used React Router for this project, with 3 routes. I
+              also used LocalStorage to persist data and used Cypress to test my
+              application with End to End testing.
             </p>
             <div className='projects-btn-container'>
               <a
